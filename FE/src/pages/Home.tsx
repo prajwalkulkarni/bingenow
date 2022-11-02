d1609f957aad4ae1d8874dc5e7863e43f7d16d5a
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-11-03 00:06:34 +0530

    Update home page

--- FE/src/pages/Home.tsx
+++ FE/src/pages/Home.tsx
@@ -5,14 +5,16 @@ import { useQuery } from 'react-query';
 import { motion, AnimatePresence } from 'framer-motion'
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import Button from '../../UI/Button';
-import CardList from '../components/CardList';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const db = getFirestore(app);
+
+const CardListLazy = React.lazy(() => import('../components/CardList'));
 const Home: React.FC = () => {
 
-    const { data:carouselData, isLoading, isError } = useQuery('carousel', async () => {
+    const { data: carouselData, isLoading, isError } = useQuery('carousel', async () => {
         const querySnapshot = await getDocs(collection(db, 'home'));
         const data = querySnapshot.docs.map((doc) => doc.data());
 
@@ -35,52 +37,53 @@ const Home: React.FC = () => {
                 <div
 
                     className='flex relative overflow-hidden flex-col justify-center min-h-[80vh]'
-                    >
+                >
                     <div className='z-10 flex justify-between px-2'>
                         <button className='text-white' onClick={() => setCount(p => p === 0 ? (p + 4) % 5 : (p - 1) % 5)}><ArrowBackIosIcon /></button>
                         <button className='text-white' onClick={() => setCount(p => (p + 1) % 5)}><ArrowForwardIosIcon /></button>
                     </div>
 
                     <AnimatePresence>
-                    <motion.div
-                    key={count}
-                    className='absolute w-full h-full'
-                    style={{ backgroundImage: `url(${require(`../assets/cover/${Math.abs(count)}.jpg`)})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}
-                    initial={{ opacity: 0 }}
-                    animate={{
-                        opacity: 1,
-                        
-                        transition: {
-                            duration: 0.8,
-                            
-                        }
-                    }}
-                    exit={{
-                        
-                        opacity: 0,
-                    }}>
-                        <div
-                            className='absolute w-full h-full p-1 bg-gradient-to-r from-black to-transparent'>
-                            <div className='flex'>
-                                <div className='flex flex-col p-6 md:w-1/2'>
-                                    <p className='text-6xl font-bold text-white'>{carouselData ? carouselData[Math.abs(count)]['Title'] : ''}</p>
-                                    <p className='p-1 text-lg text-white'>{carouselData ? carouselData[Math.abs(count)]['Plot'] : ''}</p>
+                        <motion.div
+                            key={count}
+                            className='absolute w-full h-full'
+                            style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${carouselData ? carouselData[Math.abs(count)]['Poster'] : require('../assets/cover/cover.jpg')})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}
+                            initial={{ opacity: 0 }}
+                            animate={{
+                                opacity: 1,
 
-                                    <div className='flex flex-row items-center justify-start mt-4'>
-                                        <p className='px-1 text-lg font-semibold text-white'>Genre: {carouselData ? carouselData[Math.abs(count)]['Genre'] : ''}</p>
-                                        <p className='px-1 text-lg font-semibold text-white'>|</p>
-                                        <p className='px-1 text-lg font-semibold text-white'>Year: {carouselData ? carouselData[Math.abs(count)]['Year'] : ''}</p>
-                                        <p className='px-1 text-lg font-semibold text-white'>|</p>
-                                        <p className='px-1 text-lg font-semibold text-white'><AccessTimeIcon/> {carouselData ? carouselData[Math.abs(count)]['Runtime'] : ''}</p>
+                                transition: {
+                                    duration: 0.8,
+
+                                }
+                            }}
+                            exit={{
+
+                                opacity: 0,
+                            }}>
+                            <div
+                                className='absolute w-full h-full p-1 bg-gradient-to-r from-black to-transparent'>
+                                <div className='flex'>
+                                    <div className='flex flex-col p-6 md:w-1/2'>
+                                        {carouselData ? <p className='text-6xl font-bold text-white'>{carouselData[Math.abs(count)]['Title']}</p> : <div className='min-w-[80vw] md:w-full h-6 m-2 text-6xl font-bold bg-gray-600 animate-pulse'></div>}
+                                        {carouselData ? <p className='p-1 text-lg text-white'>{carouselData[Math.abs(count)]['Plot']}</p> : <div className='min-w-[80vw] md:w-full h-8 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
+
+                                        <div className='flex flex-row items-center justify-start mt-4'>
+                                            {carouselData ? <p className='px-1 text-lg font-semibold text-white'>Genre: {carouselData[Math.abs(count)]['Genre']}</p> : <div className='w-full h-4 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
+                                            <p className='px-1 text-lg font-semibold text-white'>|</p>
+                                            {carouselData ? <p className='px-1 text-lg font-semibold text-white'>Year: {carouselData[Math.abs(count)]['Year']}</p> : <div className='w-full h-4 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
+                                            <p className='px-1 text-lg font-semibold text-white'>|</p>
+                                            {carouselData ? <p className='px-1 text-lg font-semibold text-white'>Runtime: {carouselData[Math.abs(count)]['Runtime']}</p> : <div className='w-full h-4 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
+                                        </div>
+
+                                        <Button
+                                        ><PlayCircleOutlineIcon />Watch</Button>
+
                                     </div>
 
-                                    <Button className='z-10 flex items-center px-4 py-2 mt-4 font-bold text-white bg-yellow-400 rounded-md w-fit hover:bg-yellow-500'>Watch now</Button>
-
                                 </div>
-
                             </div>
-                        </div>
-                    </motion.div>
+                        </motion.div>
                     </AnimatePresence>
 
                 </div>
@@ -88,16 +91,21 @@ const Home: React.FC = () => {
             </section>
 
             <div className='flex flex-col p-2'>
-                <p className='text-6xl font-bold'>Action & Drama</p>
-                <CardList category='action'/>
+                <p className='py-3 text-5xl font-bold'>Action & Drama</p>
+                {isLoading ? <div className='flex justify-center'><CircularProgress /></div> : <CardListLazy category='action' />}
             </div>
 
             <div className='flex flex-col p-2'>
-                <p className='text-2xl bold'>Top IMDB rated TV shows</p>
-                <div className='flex'>
-
-                </div>
+                <p className='py-3 text-5xl font-bold'>Popular animated films</p>
+                {isLoading ? <div className='flex justify-center'><CircularProgress /></div> : <CardListLazy category='animated' />}
             </div>
+
+            <div className='flex flex-col p-2'>
+                <p className='py-3 text-5xl font-bold'>Watch with friends</p>
+                {isLoading ? <div className='flex justify-center'><CircularProgress /></div> : <CardListLazy category='friends' />}
+            </div>
+
+
         </div>
     );
 }
