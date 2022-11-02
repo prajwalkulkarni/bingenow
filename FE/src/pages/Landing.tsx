5e443ee0ad03ba9a81c1547aec3f276daa5975a5
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-11-03 00:08:28 +0530

    Update unauth pages

--- FE/src/pages/Landing.tsx
+++ FE/src/pages/Landing.tsx
@@ -1,102 +1,158 @@
 import React from 'react'
 import Typed from 'react-typed'
 import { motion } from 'framer-motion'
-
+import Feature from '../components/Feature'
+import Card from '../../UI/Card'
 const Landing: React.FC = () => {
 
     return (
         <main>
-            <section>
-                <div className='flex flex-col items-center justify-center h-screen'>
+            <section className='p-0 m-0'>
+                <Feature image='cover'>
+                    <div className='flex flex-col items-center justify-center h-screen text-white'>
 
-                    <p className='font-bold text-7xl'>Bingenow</p>
+                        <p className='text-4xl font-bold sm:text-7xl'>Bingenow</p>
 
-                    <p className='py-4 text-3xl font-semibold'>Stream great <Typed
-                        strings={[
-                            "Movies",
-                            "TV shows",
-                            "Web series",
-                        ]}
-                        typeSpeed={50}
-                        backSpeed={100}
-                        loop
-                    /></p>
-                    <p className='text-3xl font-semibold'>All at one place</p>
+                        <p className='py-4 text-2xl font-semibold sm:text-3xl'>Stream great <Typed
+                            strings={[
+                                "Movies",
+                                "TV shows",
+                                "Web series",
+                            ]}
+                            typeSpeed={50}
+                            backSpeed={100}
+                            loop
+                        /></p>
+                        <p className='text-2xl font-semibold sm:text-3xl'>All at one place</p>
 
-                </div>
+                    </div>
+                </Feature>
 
             </section>
             <section>
-                <div className='grid grid-cols-1 p-3 md:grid-cols-2'>
-                    <motion.div
-                        initial="hidden"
-                        whileInView="visible"
-                        viewport={{ once: true, amount: 0.5 }}
-                    // transition={{ duration: 0.5 }}
-                    // variants={{
-                    //     visible: { opacity: 1, scale: 1 },
-                    //     hidden: { opacity: 0, scale: 0 }
-                    //   }}
-
-
-                    >
+                <Card className='my-4'>
+                    <div className='grid grid-cols-1 p-3 md:grid-cols-2'>
                         <motion.div
-                            variants={{
-                                visible: {
-                                    x: 0,
-                                    transition: {
-                                        duration: 0.5
-                                    }
-                                },
-                                hidden: { x: -500 }
-                            }}>
-                            <p className='text-2xl font-semibold'>No limit. No cost</p>
-                            <p className='text-xl'>Watch unlimited movies, TV shows & web series for FREE.</p>
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true, amount: 0.5 }}
+                        // transition={{ duration: 0.5 }}
+                        // variants={{
+                        //     visible: { opacity: 1, scale: 1 },
+                        //     hidden: { opacity: 0, scale: 0 }
+                        //   }}
+
+
+                        >
+                            <motion.div
+                                variants={{
+                                    visible: {
+                                        x: 0,
+                                        transition: {
+                                            duration: 0.5
+                                        }
+                                    },
+                                    hidden: { x: -500 }
+                                }}
+                                className='flex flex-col justify-center h-full'>
+                                <p className='text-4xl font-semibold'>No limit. No cost</p>
+                                <p className='text-xl'>Watch unlimited movies, TV shows & web series for FREE.</p>
+                            </motion.div>
+
+
                         </motion.div>
-                    </motion.div>
 
-                    <div className='visuals'>
-
-                    </div>
-                </div>
-                {/* <p>Bingenow is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!</p> */}
-
-            </section>
-            <section>
-                <div className='grid grid-cols-1 p-3 md:grid-cols-2'>
-
-
-                    <div className='visuals'>
-                        <p>Lorem ipsum dumb text</p>
-                    </div>
-
-                    <motion.div
-                        initial="hidden"
-                        whileInView="visible"
-                        viewport={{ once: true, amount: 0.5 }}
-                        variants={{
-                            hidden:{
-                                overflow: 'hidden',
-                            }
-                        }}
-                    >
-                        <motion.div
-                            variants={{
-                                visible: {
-                                    x: 0,
-                                    transition: {
-                                        duration: 0.5,
+                        
+                            <motion.div
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{ once: true, amount: 0.5 }}
+                                variants={{
+                                    hidden: {
+                                        overflow: 'hidden',
                                     }
-                                },
+                                }}
                                 
-                                hidden: { x: 500}
-                            }}>
+                            >
+                                <motion.div
+                                    variants={{
+                                        visible: {
+                                            x: 0,
+                                            transition: {
+                                                duration: 0.5,
+                                            }
+                                        },
 
-                            <p className='text-2xl font-semibold'>Anytime. Anywhere</p>
-                            <p className='text-xl'>Access and watch your favorite shows and movies, by creating a FREE account.</p>
+                                        hidden: { x: 800 }
+                                    }}
+                                    className='flex justify-center md:justify-end'>
+
+                                    <img src={require('../assets/cover/card1.jpg')} className='w-full md:w-4/5'/>
+                                </motion.div>
+                            </motion.div>
+                        
+                    </div>
+                </Card>
+                {/* <p>Bingenow is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!</p> */}
+
+            </section>
+            <section>
+                <Card className='my-4'>
+                    <div className='grid grid-cols-1 p-3 md:grid-cols-2'>
+
+                        <motion.div
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true, amount: 0.5 }}
+
+                        >
+                            <motion.div
+                                variants={{
+                                    visible: {
+                                        x: 0,
+                                        transition: {
+                                            duration: 0.5
+                                        }
+                                    },
+                                    hidden: { x: -800 }
+                                }}
+                                className='flex justify-center md:justify-start'>
+                                <img src={require('../assets/cover/card2.jpg')} className='w-full md:w-4/5'/>
+                            </motion.div>
+
+
                         </motion.div>
-                    </motion.div>
-                </div>
+
+
+                        <motion.div
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true, amount: 0.5 }}
+                            variants={{
+                                hidden: {
+                                    overflow: 'hidden',
+                                }
+                            }}
+                        >
+                            <motion.div
+                                variants={{
+                                    visible: {
+                                        x: 0,
+                                        transition: {
+                                            duration: 0.5,
+                                        }
+                                    },
+
+                                    hidden: { x: 800 }
+                                }}
+                                className='flex flex-col justify-center h-full'>
+
+                                <p className='text-4xl font-semibold'>Anytime. Anywhere</p>
+                                <p className='text-xl'>Access and watch your favorite shows and movies, by creating a FREE account.</p>
+                            </motion.div>
+                        </motion.div>
+                    </div>
+                </Card>
                 {/* <p>Bingenow is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!</p> */}
 
             </section>
@@ -104,20 +160,23 @@ const Landing: React.FC = () => {
             <section>
 
 
-                <motion.div className='flex flex-col items-center'
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true, amount: 0.5 }}
-                transition={{ duration: 0.5 }}
-                variants={{
-                    visible: { opacity: 1, scale: 1 },
-                    hidden: { opacity: 0, scale: 0 }
-                }}>
-                    <p className='text-2xl font-semibold'>Watch on Any Device</p>
-                    <p className='text-xl'>Bingenow works well on all sizes of devices. Enjoy your content on both large displays and small devices, such as smartphones.</p>
+                <Card className='my-4'>
+                    <motion.div className='flex flex-col items-center p-4'
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ duration: 0.5 }}
+                        variants={{
+                            visible: { opacity: 1, scale: 1 },
+                            hidden: { opacity: 0, scale: 0 }
+                        }}>
+                        <p className='text-4xl font-semibold'>Watch on Any Device</p>
+                        <p className='text-xl'>Bingenow works well on all sizes of devices. Enjoy your content on both large displays and small devices, such as smartphones.</p>
 
-                </motion.div>
+                        <img src={require('../assets/cover/card3.jpg')} className='object-contain' />
+                    </motion.div>
 
+                </Card>
             </section>
         </main>
     )
--- FE/src/pages/Login.tsx
+++ FE/src/pages/Login.tsx
@@ -1,15 +1,29 @@
 import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { emailRegEx } from '../CONSTANTS'
 import { app } from '../firebase'
 import { useMutation } from 'react-query'
 import useAuth from '../hooks/useAuth'
-
+import { motion } from 'framer-motion'
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import Button from '../../UI/Button'
+import Feature from '../components/Feature'
 
 const provider = new GoogleAuthProvider();
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref,
+) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 const Login: React.FC<{}> = () => {
 
+
+    const [open, setOpen] = useState(false);
     const auth = getAuth(app)
     const { val: password, isInvalid, isValid, onBlurHandler, onFocusHandler, onChangeHandler } = useAuth((val: string) => val.length >= 8)
     const { val: email, isInvalid: emailIsInvalid, isValid: emailIsValid, onBlurHandler: emailOnBlurHandler,
@@ -17,18 +31,30 @@ const Login: React.FC<{}> = () => {
 
     const formIsValid = isValid && emailIsValid
 
+    const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
     const loginHandler = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
         try {
             if (formIsValid) {
+
                 const userCredential = await signInWithEmailAndPassword(auth, email, password)
+
                 return userCredential
             }
         }
         catch (error: any) {
             const errorCode = error.code;
             const errorMessage = error.message;
+            setOpen(true)
+            return errorMessage
         }
     }
 
@@ -47,55 +73,79 @@ const Login: React.FC<{}> = () => {
             // The email of the user's account used.
             const email = error.customData.email;
             // The AuthCredential type that was used.
+            setOpen(true)
             const credential = GoogleAuthProvider.credentialFromError(error);
         }
 
     }
 
 
-    const { isLoading, error, data, mutate } = useMutation(loginHandler)
+    const { isLoading, error, data, mutate } = useMutation(loginHandler, {
+        mutationKey: 'login',
+    })
+
     return (
-        <div className='flex items-center justify-center min-h-[80vh]'>
+        <Feature image='cover'>
+            <div className='flex items-center justify-center min-h-[80vh]'>
 
-            <div className='w-1/2 p-3 bg-white rounded-lg md:w-1/4'>
-                <form onSubmit={mutate}
-                    className='flex flex-col'>
+                <Snackbar open={open} autoHideDuration={6000}
+                    onClose={handleClose}
+                    anchorOrigin={{ horizontal: 'center', vertical: 'top' }}>
+                    <Alert severity="error" sx={{ width: '100%' }}>
+                        {data}
+                    </Alert>
+                </Snackbar>
+                <div className='w-3/4 px-4 py-3 bg-white rounded-lg sm:mx-auto sm:w-full sm:max-w-md'>
+                    <form onSubmit={mutate}
+                        className='flex flex-col'>
 
-                    <div className='flex flex-col py-2'>
-                        <label htmlFor="email">Email*</label>
-                        <input className='p-1 border rounded-md border-slate-400 focus:border-violet-800 focus:outline-none' type="email"
-                            placeholder='Your email address' id="email"
-                            value={email}
-                            onChange={emailOnChangeHandler}
-                            onBlur={emailOnBlurHandler}
-                            onFocus={emailOnFocusHandler}
-                            required />
-                        {emailIsInvalid && <p className='text-red-500'>Email is invalid</p>}
-                    </div>
-                    <div className='flex flex-col py-2'>
-                        <label htmlFor="password">Password*</label>
-                        <input type="password"
-                            className='p-1 border rounded-md border-slate-400 focus:border-violet-800 focus:outline-none'
-                            placeholder='Password'
-                            value={password}
-                            onBlur={onBlurHandler}
-                            onFocus={onFocusHandler}
-                            onChange={onChangeHandler}
-                            name="password" id="password"
-                            required />
-                        {isInvalid && <p className='text-red-500'>Password must be at least 8 characters</p>}
-                    </div>
-                    <button type="submit" className='p-2 text-white rounded-md bg-violet-800 hover:bg-violet-900'>Login</button>
-                    <p className='py-1 text-sm text-gray-400'>Don't have an account? <Link to='/signup'>Signup</Link></p>
-                </form>
+                        <div className='flex flex-col py-2'>
+                            <label htmlFor="email">Email*</label>
+                            <input className='p-1 border rounded-md border-slate-400 focus:border-violet-800 focus:outline-none' type="email"
+                                placeholder='Your email address' id="email"
+                                value={email}
+                                onChange={emailOnChangeHandler}
+                                onBlur={emailOnBlurHandler}
+                                onFocus={emailOnFocusHandler}
+                                required />
+                            {emailIsInvalid && <p className='text-red-500'>Email is invalid</p>}
+                        </div>
+                        <div className='flex flex-col py-2'>
+                            <label htmlFor="password">Password*</label>
+                            <input type="password"
+                                className='p-1 border rounded-md border-slate-400 focus:border-violet-800 focus:outline-none'
+                                placeholder='Password'
+                                value={password}
+                                onBlur={onBlurHandler}
+                                onFocus={onFocusHandler}
+                                onChange={onChangeHandler}
+                                name="password" id="password"
+                                required />
+                            {isInvalid && <p className='text-red-500'>Password must be at least 8 characters</p>}
+                        </div>
+                        <Button type="submit" className='w-full'>
 
-                <hr className='h-px py-1'/>
-                <div 
-                className='flex items-center justify-center py-2 space-x-2 border border-gray-400 rounded-md cursor-pointer'
-                onClick={socialLogin}>Signin with Google account</div>
+                            {isLoading ?
+                                <motion.div
+                                    animate={{
+                                        transform: 'rotate(360deg)',
+                                        transition: { duration: 1, repeat: Infinity, repeatType: 'loop' }
+
+                                    }}
+
+                                    className='w-6 h-6 border-t-2 border-l-2 border-white rounded-xl'></motion.div> : 'Login'}
+                        </Button>
+                        <p className='py-1 text-sm text-gray-400'>Don't have an account? <Link to='/signup'>Signup</Link></p>
+                    </form>
+
+                    <hr className='h-px py-1' />
+                    <div
+                        className='flex items-center justify-center py-2 space-x-2 border border-gray-400 rounded-md cursor-pointer'
+                        onClick={socialLogin}>Signin with Google account</div>
+                </div>
+
             </div>
-
-        </div>
+        </Feature>
 
     )
 }
