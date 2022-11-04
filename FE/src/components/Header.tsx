b89c7669c8694e050e585d021e719109b1a60eac
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-11-04 23:05:07 +0530

    Conditionally render different navbar options

--- FE/src/components/Header.tsx
+++ FE/src/components/Header.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React,{useContext, useEffect} from "react";
 import {Link} from 'react-router-dom'
+import Button from "../../UI/Button";
 import Search from '../components/Search'
+import Context from "../context/Context";
+import LockIcon from '@mui/icons-material/Lock';
+
 const Header: React.FC<{}> = (props) => {
+
+    const ctx = useContext(Context)
+    const signoutHandler = () => {
+        localStorage.removeItem('auth')
+        localStorage.removeItem('username')
+        ctx.setAuth(false)
+        ctx.setUsername(null)
+    }
+    useEffect(()=>{
+
+        ctx.setAuth(JSON.parse(localStorage.getItem('auth') as string)??false)
+        ctx.setUsername(JSON.parse(localStorage.getItem('username') as string)??null)
+    },[])
 
     return (
         <div className="flex justify-between w-full p-4 bg-gray-100">
             <p>Logo</p>
-            <Search />
-            <Link to='/login' className="p-2 text-white rounded-md bg-violet-800 hover:bg-violet-900">Sign in</Link>
+            {ctx.auth && <Search />}
+            {!ctx.auth && <Link to='/login' className="p-2 font-bold text-white rounded-md bg-violet-800 hover:bg-violet-900"><LockIcon/>Sign in</Link>}
+            {ctx.auth && <Button onClick={signoutHandler}><LockIcon/>Sign out</Button>}
         </div>
     )
 }
