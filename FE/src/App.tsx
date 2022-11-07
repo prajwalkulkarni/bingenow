5017095faedaf234a8478e0fd9fec2d22252a1bb
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-11-07 14:53:53 +0530

    Wrap root component with context provider

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,32 +1,47 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useContext } from "react"
 import Landing from "./pages/Landing"
 import Header from "./components/Header"
-import { Routes, Route, BrowserRouter } from "react-router-dom"
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import { QueryClientProvider, QueryClient } from "react-query"
 import Home from "./pages/Home"
 import Movie from "./pages/Movie"
-
+import Context from "./context/Context"
 const queryClient = new QueryClient()
+let routes;
 const App: React.FC = () => {
+
+    const ctx = useContext(Context)
+
+    if(JSON.parse(localStorage.getItem('auth') as string) || ctx.auth) {
+        routes = (
+            
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:mediaType/:imdbID/" element={<Movie/>} />
+                <Route path="*" element={<Navigate to='/' replace/>} />
+            </Routes>
+        
+        )
+    }
+     else {
+        routes = (
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="*" element={<Navigate to='/' replace/>} />
+            </Routes>
+        )
+    }
     
     return (
         <Fragment>
             <QueryClientProvider client={queryClient}>
                 <BrowserRouter>
                     <Header />
-
-
-
-
-                    <Routes>
-                        <Route path="/" element={<Landing />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/signup" element={<Signup />} />
-                        <Route path="/home" element={<Home />} />
-                        <Route path='/:mediaType/:imdbID' element={<Movie/>} />
-                    </Routes>
+                    {routes}
                 </BrowserRouter>
             </QueryClientProvider>
         </Fragment>
--- FE/src/index.tsx
+++ FE/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createRoot} from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ContextProvider } from './context/Context';
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
-root.render(<App />);
+root.render(<ContextProvider><App /></ContextProvider>);
