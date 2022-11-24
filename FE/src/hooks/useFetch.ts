aa614cdfca8733442a23345a66c919f9bbf366d9
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-11-24 23:51:51 +0530

    Add usefetch custom hook

---
+++ FE/src/hooks/useFetch.ts
@@ -0,0 +1,23 @@
+import { useMutation, useQuery } from "react-query"
+
+const useFetch = (options: RequestInit = {}, queryType:string) => {
+
+    
+    async function submitHandler() {
+        const res = await fetch('http://localhost:3000/graphql', options)
+        return res.json()
+    }
+
+    if(queryType==='query'){
+        const { data, isLoading, error } = useQuery('fetch', submitHandler)
+        return { isLoading, error, data,mutate:():any=>{} }
+    }
+    else{
+        
+        const { isLoading, error, data, mutate } = useMutation(submitHandler)
+        return { isLoading, error, data, mutate }
+    }
+    
+}
+
+export default useFetch
