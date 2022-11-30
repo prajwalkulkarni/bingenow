9b245ec9be2699a960d97515b849099bcf2be0e1
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-12-01 01:00:46 +0530

    Make navbar responsive

--- FE/src/components/Header.tsx
+++ FE/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import React,{useContext, useEffect} from "react";
-import {Link} from 'react-router-dom'
+import React, { useContext, useEffect } from "react";
+import { Link } from 'react-router-dom'
 import Button from "../../UI/Button";
 import Search from '../components/Search'
 import Context from "../context/Context";
@@ -8,36 +8,88 @@ import LockIcon from '@mui/icons-material/Lock';
 const Header: React.FC<{}> = (props) => {
 
     const ctx = useContext(Context)
+
+    const [hideMobileMenu, setToggleMobileMenu] = React.useState<boolean>(true)
     const signoutHandler = () => {
         localStorage.removeItem('auth')
         localStorage.removeItem('username')
         ctx.setAuth(false)
         ctx.setUsername(null)
     }
-    useEffect(()=>{
+    useEffect(() => {
 
-        ctx.setAuth(JSON.parse(localStorage.getItem('auth') as string)??false)
-        ctx.setUsername(JSON.parse(localStorage.getItem('username') as string)??null)
-    },[])
+        ctx.setAuth(JSON.parse(localStorage.getItem('auth') as string) ?? false)
+        ctx.setUsername(JSON.parse(localStorage.getItem('username') as string) ?? null)
+    }, [])
 
     return (
-        <div className="flex justify-between w-full p-4 bg-gray-100">
-            <p>Logo</p>
-            {ctx.auth && <>
-                <Search />
-                <div className="flex items-center text-violet-800">
-                    <Link className="px-2 transition duration-500 hover:border-b-2 hover:border-violet-800" to='/'>Movies</Link>
-                    <Link className="px-2 transition duration-500 hover:border-b-2 hover:border-violet-800" to='/shows'>Series</Link>
-                    <Link className="px-2 transition duration-500 hover:border-b-2 hover:border-violet-800" to='/mywatchlist'>Watchlist</Link>
-                    <Button className='ml-2' onClick={signoutHandler}><LockIcon/>Sign out</Button>
+
+        <nav className="bg-white shadow-lg">
+            <div className="w-full">
+                <div className="flex justify-between w-full p-4 bg-gray-100">
+
+                    <p className="hidden md:inline">Logo</p>
+
+                    <div className="flex items-center px-2 md:hidden">
+                        <button className="outline-none mobile-menu-button" onClick={()=>setToggleMobileMenu(prev=>!prev)}>
+                            <svg className="w-6 h-6 text-gray-500 hover:text-purple-800"
+                                x-show="!showMenu"
+                                fill="none"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path d="M4 6h16M4 12h16M4 18h16"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    {ctx.auth && <>
+                        <Search />
+                        <div className="items-center hidden md:flex text-violet-800">
+                            <Link className="px-2 transition duration-500 hover:border-b-2 hover:border-violet-800" to='/'>Movies</Link>
+                            <Link className="px-2 transition duration-500 hover:border-b-2 hover:border-violet-800" to='/shows'>Series</Link>
+                            <Link className="px-2 transition duration-500 hover:border-b-2 hover:border-violet-800" to='/mywatchlist'>Watchlist</Link>
+                            <Button className='ml-2' onClick={signoutHandler}><LockIcon />Sign out</Button>
+                        </div>
+                    </>}
+                    {!ctx.auth && <Link to='/login' className="p-2 font-bold text-white rounded-md bg-violet-800 hover:bg-violet-900"><LockIcon />Sign in</Link>}
+
                 </div>
-            </>}
-            {!ctx.auth && <Link to='/login' className="p-2 font-bold text-white rounded-md bg-violet-800 hover:bg-violet-900"><LockIcon/>Sign in</Link>}
+            </div>
 
+            <div className={`${hideMobileMenu?'hidden ':''}mobile-menu transition-all`}>
+                <ul className="md:hidden">
+                    <li className="active"><Link to="/" className="block px-2 py-4 text-sm text-center hover:text-white hover:bg-violet-800">Movies</Link></li>
+                    <li><Link to="/shows" className="block px-2 py-4 text-sm text-center transition duration-300 hover:bg-violet-800 hover:text-white">Series</Link></li>
+                    <li><Link to="/mywatchlist" className="block px-2 py-4 text-sm text-center transition duration-300 hover:bg-violet-800 hover:text-white">Watchlist</Link></li>
+                    <li><Button className='flex justify-center w-full rounded-none' onClick={signoutHandler}><LockIcon />Sign out</Button></li>
+                </ul>
+            </div>
 
-        </div>
+        </nav>
     )
 }
 
 
 export default Header
+
+
+/*
+<div className="flex flex-wrap justify-between w-full p-4 bg-gray-100">
+                <p>Logo</p>
+                {ctx.auth && <>
+                    <Search />
+                    <div className="flex items-center text-violet-800">
+                        <li><Link className="px-2 transition duration-500 hover:border-b-2 hover:border-violet-800" to='/'>Movies</Link></li>
+                        <li><Link className="px-2 transition duration-500 hover:border-b-2 hover:border-violet-800" to='/shows'>Series</Link></li>
+                        <li><Link className="px-2 transition duration-500 hover:border-b-2 hover:border-violet-800" to='/mywatchlist'>Watchlist</Link></li>
+                        <Button className='ml-2' onClick={signoutHandler}><LockIcon />Sign out</Button>
+                    </div>
+                </>}
+                {!ctx.auth && <li><Link to='/login' className="p-2 font-bold text-white rounded-md bg-violet-800 hover:bg-violet-900"><LockIcon />Sign in</Link></li>}
+
+
+            </div>
+            */
