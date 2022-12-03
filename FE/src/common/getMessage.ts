674866d143aad02ee181a47361e279d2c3144dde
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-12-03 23:27:37 +0530

    Add getMessage.ts

---
+++ FE/src/common/getMessage.ts
@@ -0,0 +1,15 @@
+function getMessage(responsedata:any):[string,"success"|"error"|"info"|"warning"]{
+       if(!!responsedata){
+         
+        if(Object.keys(responsedata).includes('errors')){
+            return [responsedata.errors[0].message,"info"]
+        }
+        else{
+            return [responsedata?.data.addToWatchlist.title + ' added to watchlist',"success"]
+        }
+       }
+       return ["","info"]
+
+}
+
+export default getMessage;
