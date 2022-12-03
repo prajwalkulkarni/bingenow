0edde8289bed7d27ef44efaad0e455e89ba6c6f5
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-12-03 23:30:06 +0530

    Update button UI component

--- FE/UI/Button.tsx
+++ FE/UI/Button.tsx
@@ -14,7 +14,7 @@ const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
     return (
         <button
         {...restProps}
-        className={`flex rounded-md w-fit z-10 items-center px-4 py-2 font-bold flex justify-center p-2 text-white rounded-md bg-violet-800 hover:bg-violet-900 ${class_name??''}`}
+        className={`flex rounded-md z-10 items-center px-4 py-2 font-bold flex justify-center p-2 text-white rounded-md bg-violet-800 hover:bg-violet-900 ${class_name??''}`}
         >
         {children}
         </button>
