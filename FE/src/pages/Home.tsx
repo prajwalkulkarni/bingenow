912dd6dc938dae60af3d0dafc313d1bd7ab6781b
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-12-03 23:28:38 +0530

    Disable add to watchlist till media data is loaded

--- FE/src/pages/Home.tsx
+++ FE/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { collection, getDocs, getFirestore } from 'firebase/firestore';
 import { app } from '../firebase'
 import { useQuery } from 'react-query';
@@ -7,13 +7,26 @@ import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import Button from '../../UI/Button';
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import AlbumIcon from '@mui/icons-material/Album';
+
 import CircularProgress from '@mui/material/CircularProgress';
+import MediaPortal from '../components/MediaPortal';
+import ReactDOM from 'react-dom';
+import useFetch from '../hooks/useFetch';
+import SnackbarExtended from '../../UI/SnackbarExtended';
+import getMessage from '../common/getMessage';
 
 const db = getFirestore(app);
 
 const CardListLazy = React.lazy(() => import('../components/CardList'));
 const Home: React.FC = () => {
 
+    const [count, setCount] = useState(0)
+    const [play, setPlay] = useState(false)
+    const [open, setOpen] = useState(false);
     const { data: carouselData, isLoading, isError } = useQuery('carousel', async () => {
         const querySnapshot = await getDocs(collection(db, 'home'));
         const data = querySnapshot.docs.map((doc) => doc.data());
@@ -21,15 +34,76 @@ const Home: React.FC = () => {
         return data;
     })
 
-    const [count, setCount] = useState(0)
+    const media_data = {
+        imdbId: carouselData?.[Math.abs(count)]['imdbID'],
+        title: carouselData?.[Math.abs(count)]['Title'],
+        poster: `https://image.tmdb.org/t/p/original${carouselData?.[Math.abs(count)]['Poster']}`,
+        plot: carouselData?.[Math.abs(count)]['Plot'],
+        runtime: carouselData?.[Math.abs(count)]['Runtime'],
+        year: carouselData?.[Math.abs(count)]['Year'],
+        genre: carouselData?.[Math.abs(count)]['Genre'],
 
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         setCount(p => (p + 1) % 5)
-    //     }, 5000);
-    //     return () => clearInterval(interval);
-    // }, []);
+    }
 
+    
+    const { isLoading: addToWatchlistIsLoading, error: watchListError, data: watchListData, mutate: watchlistMutate } = useFetch({
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': '*'
+        },
+        body: JSON.stringify({
+            query: `
+            mutation {
+                addToWatchlist(item:{
+                    imdbId:"${media_data.imdbId}",
+                    title:"${media_data.title}",
+                    poster:"${media_data.poster}",
+                    plot:"${media_data.plot}",
+                    runtime:"${media_data.runtime}",
+                    year:"${media_data.year}",
+                    genre:"${media_data.genre}",
+                    media:"movie"
+                },userId:"${JSON.parse(localStorage.getItem('userId')!)}"){
+                    imdbId
+                    title
+                }
+            }
+            `
+            
+                
+        })
+    }, 'mutate')
+
+    useEffect(()=>{
+        if(!addToWatchlistIsLoading&& watchListData){
+            setOpen(true)
+        }
+    },[addToWatchlistIsLoading, watchListData])
+
+    useEffect(() => {
+        let interval: NodeJS.Timer;
+
+        if (!play) {
+            interval = setInterval(() => {
+                setCount(p => (p + 1) % 5)
+            }, 5000);
+        }
+
+        return () => clearInterval(interval);
+    }, [play]);
+
+
+    const handleClose = useCallback((event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    },[]);
+
+    const [message,severity] = getMessage(watchListData)
+    
     return (
         <div className='flex flex-col'>
             <section>
@@ -38,6 +112,7 @@ const Home: React.FC = () => {
 
                     className='flex relative overflow-hidden flex-col justify-center min-h-[80vh]'
                 >
+                    <SnackbarExtended open={open} message={message} severity={severity} handleClose={handleClose}/>
                     <div className='z-10 flex justify-between px-2'>
                         <button className='text-white' onClick={() => setCount(p => p === 0 ? (p + 4) % 5 : (p - 1) % 5)}><ArrowBackIosIcon /></button>
                         <button className='text-white' onClick={() => setCount(p => (p + 1) % 5)}><ArrowForwardIosIcon /></button>
@@ -48,36 +123,64 @@ const Home: React.FC = () => {
                             key={count}
                             className='absolute w-full h-full'
                             style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${carouselData ? carouselData[Math.abs(count)]['Poster'] : require('../assets/cover/cover.jpg')})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}
-                            initial={{ opacity: 0 }}
+                            initial={{ x: 1500 }}
                             animate={{
-                                opacity: 1,
+                                x: 0,
 
                                 transition: {
-                                    duration: 0.8,
+                                    duration: 1.3,
 
                                 }
                             }}
                             exit={{
 
-                                opacity: 0,
+                                x: -1500,
+                                transition: {
+                                    duration: 1.5,
+                                }
                             }}>
                             <div
                                 className='absolute w-full h-full p-1 bg-gradient-to-r from-black to-transparent'>
+                                {play && ReactDOM.createPortal(<MediaPortal
+                                    mediaType='movie' imdbID={carouselData ? media_data.imdbId : ''}
+                                    onClick={() => setPlay(false)} />, document.getElementById('portal')!)}
                                 <div className='flex'>
                                     <div className='flex flex-col p-6 md:w-1/2'>
-                                        {carouselData ? <p className='text-6xl font-bold text-white'>{carouselData[Math.abs(count)]['Title']}</p> : <div className='min-w-[80vw] md:w-full h-6 m-2 text-6xl font-bold bg-gray-600 animate-pulse'></div>}
-                                        {carouselData ? <p className='p-1 text-lg text-white'>{carouselData[Math.abs(count)]['Plot']}</p> : <div className='min-w-[80vw] md:w-full h-8 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
+                                        {carouselData ? <p className='text-6xl font-bold text-white'>{media_data.title}</p> : <div className='min-w-[80vw] md:w-full h-6 my-2 text-6xl font-bold bg-gray-600 animate-pulse'></div>}
+                                        {carouselData ? <p className='p-1 text-lg text-white'>{media_data.plot}</p> : <div className='min-w-[80vw] md:w-full h-8 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
 
-                                        <div className='flex flex-row items-center justify-start mt-4'>
-                                            {carouselData ? <p className='px-1 text-lg font-semibold text-white'>Genre: {carouselData[Math.abs(count)]['Genre']}</p> : <div className='w-full h-4 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
-                                            <p className='px-1 text-lg font-semibold text-white'>|</p>
-                                            {carouselData ? <p className='px-1 text-lg font-semibold text-white'>Year: {carouselData[Math.abs(count)]['Year']}</p> : <div className='w-full h-4 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
-                                            <p className='px-1 text-lg font-semibold text-white'>|</p>
-                                            {carouselData ? <p className='px-1 text-lg font-semibold text-white'>Runtime: {carouselData[Math.abs(count)]['Runtime']}</p> : <div className='w-full h-4 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
+                                        <div className='flex flex-col justify-start mt-4 md:items-center md:flex-row'>
+                                            {carouselData ? <p className='flex items-center px-1 text-lg font-semibold text-white'><AlbumIcon/> &nbsp;- {media_data.genre}</p> : <div className='w-full h-4 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
+                                            <p className='hidden text-lg font-semibold text-white md:flex sm:px-1 md:px-1'>|</p>
+                                            {carouselData ? <p className='flex items-center px-1 text-lg font-semibold text-white'><CalendarMonthIcon/> &nbsp;- {media_data.year}</p> : <div className='w-full h-4 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
+                                            <p className='hidden text-lg font-semibold text-white md:flex sm:px-1 md:px-1'>|</p>
+                                            {carouselData ? <p className='flex items-center px-1 text-lg font-semibold text-white'><AccessTimeIcon/> &nbsp;- {media_data.runtime}</p> : <div className='w-full h-4 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
                                         </div>
 
-                                        <Button
-                                        ><PlayCircleOutlineIcon />Watch</Button>
+                                        <div className='flex'>
+                                            {
+                                                carouselData ?
+                                                <>
+                                                <Button
+                                                className='mx-1'
+                                                onClick={() => setPlay(true)}><PlayCircleOutlineIcon />Watch</Button>
+
+                                            <Button
+                                                className='mx-1' onClick={watchlistMutate}>
+                                                {addToWatchlistIsLoading ?
+                                                    <motion.div
+                                                        animate={{
+                                                            transform: 'rotate(360deg)',
+                                                            transition: { duration: 1, repeat: Infinity, repeatType: 'loop' }
+
+                                                        }}
+
+                                                        className='w-6 h-6 border-t-2 border-l-2 border-white rounded-xl'></motion.div> : 'Add to watchlist'}
+                                            </Button>
+                                                </>:
+                                                <div className='w-full h-8 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>
+                                            }
+                                        </div>
 
                                     </div>
 
--- FE/src/pages/Movie.tsx
+++ FE/src/pages/Movie.tsx
@@ -11,6 +11,9 @@ import { motion, AnimatePresence } from 'framer-motion'
 import useFetch from '../hooks/useFetch';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import SnackbarExtended from '../../UI/SnackbarExtended';
 import getMessage from '../common/getMessage'
@@ -35,7 +38,7 @@ const Movie: React.FC<{}> = (props) => {
 
     const [open, setOpen] = useState(false);
 
-    const [toast, setToast] = useState<{message:string,severity:"success" | "error" | "info" | "warning"}>({
+    const [toast, setToast] = useState<{ message: string, severity: "success" | "error" | "info" | "warning" }>({
         message: '',
         severity: 'success'
     })
@@ -56,9 +59,9 @@ const Movie: React.FC<{}> = (props) => {
         }
 
         setOpen(false);
-    },[]);
+    }, []);
 
-    
+
     const { data, status, isLoading, error } = useQuery([imdbID!], async () => {
 
         const backdrop = await fetch(`https://api.themoviedb.org/3/find/${imdbID}?api_key=${process.env.REACT_APP_TMDB_API_KEY}&external_source=imdb_id`)
@@ -139,19 +142,19 @@ const Movie: React.FC<{}> = (props) => {
         })
     }, 'mutate')
 
-    useEffect(()=>{
-        if(!addToWatchlistIsLoading&& watchListData){
+    useEffect(() => {
+        if (!addToWatchlistIsLoading && watchListData) {
             setOpen(true)
-            
-            const [message,severity] = getMessage(watchListData)
+
+            const [message, severity] = getMessage(watchListData)
             setToast({
                 message,
                 severity
             })
         }
-    },[addToWatchlistIsLoading, watchListData])
-    
-    
+    }, [addToWatchlistIsLoading, watchListData])
+
+
     return (
         <main>
             <section>
@@ -162,12 +165,12 @@ const Movie: React.FC<{}> = (props) => {
 
                     <div className='flex'>
                         <div className='flex flex-col p-6 md:w-1/2'>
-                        <SnackbarExtended open={open} message={toast.message} severity={toast.severity} handleClose={handleClose}/>
+                            <SnackbarExtended open={open} message={toast.message} severity={toast.severity} handleClose={handleClose} />
                             {data ? <p className='text-5xl font-bold text-white'>{data.title}</p> : <div className='w-full h-6 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
                             <div className='flex flex-col py-3 md:flex-row md:items-center'>
                                 {mediaType === 'series' && <FormControl sx={{ minWidth: 120 }} size="small"
-                                className='text-white border-none h-fit w-fit bg-violet-800 hover:border-none hover:text-white'>
-                                    
+                                    className='text-white border-none h-fit w-fit bg-violet-800 hover:border-none hover:text-white'>
+
                                     <Select
                                         labelId="demo-select-small"
                                         id="demo-select-small"
@@ -175,11 +178,11 @@ const Movie: React.FC<{}> = (props) => {
                                         label="Season"
                                         sx={{
                                             color: 'white',
-                                            boxShadow: 'none', 
+                                            boxShadow: 'none',
                                             '.MuiOutlinedInput-notchedOutline': { border: 0 },
                                             "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                                                border: 0,   
-                                              },
+                                                border: 0,
+                                            },
                                         }}
                                         notched={false}
                                         className='text-white bg-violet-800 hover:border-none hover:text-white'
@@ -191,11 +194,11 @@ const Movie: React.FC<{}> = (props) => {
                                     </Select>
                                 </FormControl>}
                                 {data ? <div className='flex flex-col md:flex-row'>
-                                    <p className='text-lg font-semibold text-white sm:px-1'><AccessTimeIcon /> {data.runtime}</p>
+                                    <p className='flex items-center text-lg font-semibold text-white sm:px-1'><AccessTimeIcon /> &nbsp;- {data.runtime}</p>
                                     <p className='hidden text-lg font-semibold text-white md:flex sm:px-1 md:px-1'>|</p>
-                                    <p className='text-lg font-semibold text-white sm:px-1'>IMDB Rating: {data.imdbRating}</p>
+                                    <p className='flex items-center text-lg font-semibold text-white sm:px-1'><StarOutlineIcon/> &nbsp;- IMdb {data.imdbRating}</p>
                                     <p className='hidden px-1 text-lg font-semibold text-white md:flex md: sm:px-1'>|</p>
-                                    <p className='text-lg font-semibold text-white sm:px-1'>Year: {data.year}</p></div> : <div className='w-full h-4 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
+                                    <p className='flex items-center text-lg font-semibold text-white sm:px-1'><CalendarMonthIcon/> &nbsp;- {data.year}</p></div> : <div className='w-full h-4 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
 
                             </div>
                             {data ? <p className='p-1 text-lg text-white'>{data.plot}</p> : <div className='w-full h-8 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>}
@@ -207,14 +210,16 @@ const Movie: React.FC<{}> = (props) => {
                                             <p className='text-lg font-semibold text-white sm:px-1'>Director: {data.director}</p>
                                             <p className='text-lg font-semibold text-white sm:px-1'>Actors: {data.actors}</p>
                                             <p className='text-lg font-semibold text-white sm:px-1'>Genre: {data.genre}</p>
-                                            <p className='text-lg font-semibold text-white sm:px-1'>Year: {data.year}</p>
                                             
+
                                         </> :
                                         <div className='w-full h-8 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>
                                 }
                                 {mediaType === 'movie' && <div className='flex'>
-                                <Button className='mx-1' onClick={() => setPlay(true)}>Watch now</Button>
-                                <Button
+                                    {
+                                        data ? <>
+                                            <Button className='mx-1' onClick={() => setPlay(true)}><PlayCircleOutlineIcon />Watch</Button>
+                                            <Button
                                                 className='mx-1' onClick={watchlistMutate}>
                                                 {addToWatchlistIsLoading ?
                                                     <motion.div
@@ -225,8 +230,11 @@ const Movie: React.FC<{}> = (props) => {
                                                         }}
 
                                                         className='w-6 h-6 border-t-2 border-l-2 border-white rounded-xl'></motion.div> : 'Add to watchlist'}
-                                </Button>
-                                    </div>}
+                                            </Button>
+                                        </> :
+                                            <div className='w-full h-8 text-lg font-bold text-white bg-gray-600 animate-pulse'></div>
+                                    }
+                                </div>}
                             </div>
                         </div>
                     </div>
