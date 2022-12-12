47ef7b17ee4a1d491d3ee568b1cea1fe7228d385
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-12-12 16:55:06 +0530

    Resize player on small and medium above screen

--- FE/src/components/MediaPortal.tsx
+++ FE/src/components/MediaPortal.tsx
@@ -7,9 +7,9 @@ const MediaPortal: React.FC<{mediaType:string,imdbID:string,episode?:number,seas
     
     return (
         <div className="fixed top-0 left-0 z-10 flex items-center justify-center w-screen h-screen bg-black" onClick={props.onClick}>
-            
+               <div className="fixed top-0 font-bold text-white right-5 hover:pointer" onClick={props.onClick}>X</div>
 
-                <div className="z-20 w-full bg-black h-3/4 md:w-1/2 md:h-1/2" >
+                <div className="z-20 w-full bg-black h-1/4 md:w-4/5 md:h-3/4" >
                 <iframe
                 src={`${process.env.REACT_APP_PLAYER_URL}/${mediaType==='series'?'tv':'movie'}?id=${imdbID}${mediaType==='series'?'&s='+props.season+'&e='+props.episode:''}`}
                 frameBorder="0"
