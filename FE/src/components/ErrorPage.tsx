eeb26e9e0ddcd15dc84e5ee1a1404f4c021f586e
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2023-02-16 19:30:48 +0530

    Staging (#12)
    * Format code(ErrorPage)
    
    * Update Target branch for staging deployment
    
    * Update push branch syntax
    
    * Fix branch syntax

--- FE/src/components/ErrorPage.tsx
+++ FE/src/components/ErrorPage.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-
 const ErrorPage = () => {
 
-    
-        return <div className="flex flex-col items-center w-full h-full">
-
-            <img src={require('../assets/error.png')} alt="Error"/>
-            <p className="text-2xl text-gray-500">Something went wrong</p>
-        </div>
+    return <div className="flex flex-col items-center w-full h-full">
+        <img src={require('../assets/error.png')} alt="Error" />
+        <p className="text-2xl text-gray-500">Something went wrong</p>
+    </div>
 
 
 }
