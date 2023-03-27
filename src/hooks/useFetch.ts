7caedc3308616c812c899c64d592057742b09063
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2023-03-27 22:23:56 +0530

    Revamp code to invoke lambda function

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -4,8 +4,15 @@ const useFetch = (options: RequestInit = {}, queryType:string) => {
 
     
     async function submitHandler() {
-        const res = await fetch('http://localhost:3000/graphql', options)
+        if(process.env.REACT_APP_ENV==='production'){
+            const res = await fetch('https://graphql-apollo-server.herokuapp.com/graphql', options)
+            return res.json()
+        }
+        else{
+            const res = await fetch(`${process.env.REACT_APP_BACKEND_DOMAIN}/graphql`, options)
         return res.json()
+        }
+        
     }
 
     if(queryType==='query'){
