2588fb7153120a1863ef075e2856a044119dcb78
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2023-06-18 15:16:38 +0530

    Update card content element

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -16,7 +16,7 @@ const CardItem: React.FC<CardItemProps> = (props) => {
 
   return (
     <Card className="flex-shrink-0 mx-2 basis-1/2 sm:basis-2/5 md:basis-1/5">
-      <div className="relative flex flex-wrap overflow-hidden">
+      <div className="relative flex flex-wrap overflow-hidden group">
         <figure className="relative flex flex-wrap w-100">
           <Link to={`/${!movie ? "series" : "movie"}/${imdbID}`}>
             <img
@@ -28,13 +28,13 @@ const CardItem: React.FC<CardItemProps> = (props) => {
             />
           </Link>
         </figure>
-        <div className="absolute bottom-0 left-0 z-10 w-full bg-gradient-to-t from-black to-transparent">
+        <div className="absolute bottom-0 left-0 z-10 w-full transition duration-500 translate-y-full bg-white group-hover:translate-y-0">
           <Link to={`/${!movie ? "series" : "movie"}/${imdbID}`}>
-            <p className="px-3 text-2xl font-bold text-white text-ellipsis">
+            <p className="px-3 text-2xl font-bold text-black text-ellipsis">
               {title}
             </p>
           </Link>
-          <p className="px-3 font-bold text-black text-gray-200 ">{year}</p>
+          <p className="px-3 font-bold text-black text-gray-500 ">{year}</p>
         </div>
       </div>
     </Card>
