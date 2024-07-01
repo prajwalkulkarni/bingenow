15e020e062cda1f877070257f60d57a9f4b75463
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2024-07-01 23:18:06 +0530

    Update url formatting

--- src/components/MediaPortal.tsx
+++ src/components/MediaPortal.tsx
@@ -9,12 +9,13 @@ const MediaPortal: React.FC<{
   onClick: React.MouseEventHandler<HTMLDivElement>;
 }> = (props) => {
   const { mediaType, imdbID, season, episode } = props;
-  const embed = `${mediaType === "series" ? "tv" : "movie"}?id=${imdbID}${
-    mediaType === "series" ? "&s=" + props.season + "&e=" + props.episode : ""
+
+  const vidSrc = `${
+    mediaType === "series"
+      ? "tv/" + imdbID + "/" + season + "/" + episode
+      : "movie/" + imdbID
   }`;
-  const vidSrc = `/${
-    mediaType === "series" ? imdbID + "/" + season + "-" + episode : imdbID
-  }`;
+
   return (
     <div className="fixed top-0 left-0 z-10 flex items-center justify-center w-screen h-screen bg-black">
       <div
@@ -26,7 +27,7 @@ const MediaPortal: React.FC<{
 
       <div className="z-20 w-full md:w-4/5 md:h-3/4">
         <iframe
-          src={`${process.env.REACT_APP_PLAYER_URL}/${vidSrc}/color-5b21b6`}
+          src={`${process.env.REACT_APP_PLAYER_URL}/${vidSrc}`}
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
