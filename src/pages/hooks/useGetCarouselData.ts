33f016ae34d74230af0150de136627779effd717
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2024-08-15 23:00:29 +0530

    Show trending movies and tv shows in carousel

--- src/pages/hooks/useGetCarouselData.ts
+++ src/pages/hooks/useGetCarouselData.ts
@@ -1,20 +1,59 @@
-import { collection, getDocs, getFirestore } from "firebase/firestore";
 import { useQuery } from "react-query";
-import { app } from "../../firebase";
+import { getLatestAuthToken } from "../../utils/manageToken";
 
-const db = getFirestore(app);
+export interface CarouselMovies {
+  imdbId?: string;
+
+  id: string;
+  backdrop_path: string;
+  overview: string;
+  poster_path: string;
+  release_date: Date;
+  title: string;
+  vote_average: number;
+  vote_count: number;
+  first_air_date: Date;
+}
+
+export interface CarouselSeries {
+  imdbId?: string;
+
+  id: string;
+  backdrop_path: string;
+  overview: string;
+
+  name: string;
+  vote_average: number;
+  vote_count: number;
+  first_air_date: Date;
+}
+
+interface CarouselResponse {
+  page: number;
+  results: CarouselMovies[] | CarouselSeries[];
+  total_pages: number;
+  total_results: number;
+}
+
 export const useGetCarouselData = ({ movies }: { movies: boolean }) => {
   const {
     data: carouselData,
     isLoading,
     isError,
   } = useQuery(movies ? "carousel" : "carouselshows", async () => {
-    const querySnapshot = await getDocs(
-      collection(db, movies ? "home" : "shows")
-    );
-    const data = querySnapshot.docs.map((doc) => doc.data());
+    const TARGET_ENDPOINT = movies
+      ? `https://54aoybt2ja.execute-api.ap-southeast-1.amazonaws.com/tmdb/latestpopularmovies`
+      : `https://54aoybt2ja.execute-api.ap-southeast-1.amazonaws.com/tmdb/trendingtvshows`;
 
-    return data;
+    const token = await getLatestAuthToken();
+    const latestdata = await fetch(TARGET_ENDPOINT, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+    const jsonData: CarouselResponse = await latestdata.json();
+
+    return jsonData.results.slice(0, 5);
   });
 
   return {
